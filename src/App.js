cdce6b49e919a0dd5abce5fde50ae2728338074c
dherg <hergendavid@gmail.com>
2018-04-04 23:58:59 -0400

    Geocoding

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 // my imports
 import GoogleMapReact from 'google-map-react';
 import Pin from './Pin';
-import { BrowserRouter as Router, Route, NavLink, IndexRoute, hashHistory, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const google = window.google;
@@ -47,7 +47,7 @@ class SimpleMap extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.center != nextProps.center) {
+    if (this.props.center !== nextProps.center) {
       this.setState({
         center: nextProps.center,
       });
@@ -123,6 +123,8 @@ class SearchBar extends Component {
 
   constructor(props) {
     super(props);
+
+    this.changeMapCenter = this.changeMapCenter.bind(this);
     this.geocodeAddress = this.geocodeAddress.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -132,31 +134,12 @@ class SearchBar extends Component {
     }
   }
 
-  // componentDidMount() {
+  // take in geometry object and update the map center with .location and .viewport
+  changeMapCenter(geometry) {
+    console.log('@changeMapCenter: geometry: ' + geometry);
+    console.log('geometry.location: ' + geometry.location);
+    console.log('geometry.viewport: ' + geometry.viewport);
 
-  //   var geocoder = new google.maps.Geocoder();
-
-  //   // document.getElementById('submit').addEventListener('click', function() {
-  //   //   this.geocodeAddress(geocoder);
-  //   // });
-  // }
-
-  geocodeAddress(address) {
-    var geocoder = new google.maps.Geocoder();
-    var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
-      console.log('Results: ' + results[0]);
-      if (status == 'OK') {
-        console.log('status OK. results: ' + console.log(results[0].formatted_address));
-        // TODO: set center of map here
-        // map.setCenter(results[0].geometry.location);
-        return(results[0].geometry.location)
-      } else {
-        console.log('Geocode was not successful. ')
-        console.log('Status: ' + status);
-        return(null)
-      }
-    }); 
   }
 
   // handle change in text box
@@ -164,17 +147,39 @@ class SearchBar extends Component {
     this.setState({text: event.target.value});
   }
 
+  geocodeAddress(address) {
+    var geocoder = new google.maps.Geocoder();
+    var self = this;
+    geocoder.geocode({'address': address}, function(results, status) {
+      console.log('Results: ' + results[0]);
+      if (status === 'OK') {
+        console.log('status OK. results: ' + results[0].formatted_address);
+        console.log('results[0].geometry.location: ' + results[0].geometry.location);
+        // TODO: set center of map here. pass results[0].geometry (which contains location and bounds)
+        // https://developers.google.com/maps/documentation/javascript/geocoding
+        self.changeMapCenter(results[0].geometry);
+      } else {
+        console.log('Geocode was not successful.');
+        console.log('Status: ' + status);
+      }
+    }); 
+  }
+
   // handle clicking the "submit" button
   handleSubmit() {
-    var location = this.geocodeAddress(this.state.text); // location = results[0].geometry.location
-    console.log('geocoded');
-    if (location != null) {
-      // set center of the map to the location
-      this.props.changeMapCenter(location);
-    } else {
-      // TODO: display try a different search message
-      console.log('ERROR: TRY DIFFERENT SEARCH')
-    }
+    this.geocodeAddress(this.state.text);
+
+    // var location = this.geocodeAddress(this.state.text); // location = results[0].geometry
+    // console.log('location: ' + location);
+    // if (location != null && location !== undefined) {
+    //   console.log('geocoded');
+    //   console.log(location.location);
+    //   // set center of the map to the location
+    //   this.props.changeMapCenter(location);
+    // } else {
+    //   // TODO: display try a different search message
+    //   console.log('ERROR: TRY DIFFERENT SEARCH')
+    // }
 
   }
 
@@ -241,7 +246,7 @@ class InfoWindow extends Component {
   componentDidMount() {
     // TODO: fetch pin info
     const spotifyembed = (
-      <iframe src="https://open.spotify.com/embed/track/07gqJjvwwuZ1assFLKbiNn" width="250" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe src="https://open.spotify.com/embed/track/07gqJjvwwuZ1assFLKbiNn" width="250" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Spotify Player"></iframe>
       );
     const title = "Dance Music";
     const artist = "The Mountain Goats";
