05b51a2bdc763fd675ad3d940003e61e14da4d98
dherg <hergendavid@gmail.com>
2019-06-06 22:13:14 -0400

    update map styling

--- client/src/SimpleMap.js
+++ client/src/SimpleMap.js
@@ -79,7 +79,132 @@ export default class SimpleMap extends Component {
     }
   }
 
+  createMapOptions(maps) {
+    return({
+      streetViewControl: true, 
+      mapTypeControl: true,
+      styles: [
+            {
+                "featureType": "landscape",
+                "stylers": [
+                    {
+                        "hue": "#FFBB00"
+                    },
+                    {
+                        "saturation": 43.400000000000006
+                    },
+                    {
+                        "lightness": 37.599999999999994
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "stylers": [
+                    {
+                        "hue": "#FFC200"
+                    },
+                    {
+                        "saturation": -61.8
+                    },
+                    {
+                        "lightness": 45.599999999999994
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "stylers": [
+                    {
+                        "hue": "#FF0300"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 51.19999999999999
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "stylers": [
+                    {
+                        "hue": "#FF0300"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 52
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "stylers": [
+                    {
+                        "hue": "#0078FF"
+                    },
+                    {
+                        "saturation": -13.200000000000003
+                    },
+                    {
+                        "lightness": 2.4000000000000057
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "stylers": [
+                    {
+                        "hue": "#00FF6A"
+                    },
+                    {
+                        "saturation": -1.0989010989011234
+                    },
+                    {
+                        "lightness": 11.200000000000017
+                    },
+                    {
+                        "gamma": 1
+                    }
+                ]
+            }
+      ]
+    });
+  }
+
   render() {
+
+    // var styledMapType = new google.maps.StyledMapType(
+    //   [
+
+    //   ]
+    //   {name: 'Styled Map'});
+
+    // createMapOptions(maps) {
+    //   return({
+    //     streetViewControl: true, 
+    //     mapTypeControl: true
+    //   });
+    // }
+
 
     return (
       <div className="SimpleMap">
@@ -92,7 +217,7 @@ export default class SimpleMap extends Component {
             v: '3.30'
           }}
           onClick={this.addPin}
-          options={{streetViewControl: true}}
+          options={this.createMapOptions}
         >
           {this.pinListToComponents(this.state.pinList)}
         </GoogleMapReact>
